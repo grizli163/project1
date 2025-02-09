aacc6e5e644190d8f81204e4b1d1af5b21317db7
Roman Bakhtinov <romanbahtinov@gmail.com>
2025-02-09 22:10:50 +0400

    version 0.0.1

---
+++ test_code.py
@@ -0,0 +1,12 @@
+import time
+
+
+def print_numbers():
+    while True:
+        for i in range(1, 11):
+            print(i, flush=True)
+            time.sleep(1)  # Задержка в 1 секунду
+
+
+if __name__ == "__main__":
+    print_numbers()
